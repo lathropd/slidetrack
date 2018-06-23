871ba2c773f43dc642363f3b1235c94925deb451
Daniel Lathrop <daniel.lathrop@gmail.com>
2018-06-23 14:49:07 -0500

    Create genericTest.js tests file to make sure test setup is running

---
+++ test/genericTest.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const chai = require("chai")
+const assert = chai.assert;
+
+// demo tests from https://www.sitepoint.com/unit-test-javascript-mocha-chai/
+
+describe('Array', function() {
+  it('should start empty', function () {
+    var arr = new Array();
+
+    assert.equal(arr.length, 0);
+    assert.equal(arr.toString(), "");
+  });
+});
+
+
+
+
