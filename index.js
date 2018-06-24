ce45449c280f220304ce1d5e39139b03e0c2f5c4
Daniel Lathrop <daniel.lathrop@gmail.com>
2018-06-23 20:16:22 -0500

    Move to building all styles in slideyslides.scss so I can use flexbox and other modern stuff. Sad to see you go reveal.scss

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//// simple code to calculate height offset for each section element
+//var slideHeight = document.getElementsByClassName("slides")[0].offsetHeight;
+//
+//var sections = document.getElementsByTagName("section");
+//for (section in sections) {
+//    console.log(section);
+//    var secHeight = section.offsetHeight;
+//    var secOffset = (slideHeight - secHeight)/2;
+//    if (secOffset < 1) { secOffset = 0}
+//    section.style.top = secOffset+"px"
+//}
+
+console.log("slidey");
+
+const slidey = {
+    run: () => console.log("slide!"),
+
+}
