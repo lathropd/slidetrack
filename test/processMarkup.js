7536071856349bc79ba28a48c75e26774647aed5
Daniel Lathrop <daniel.lathrop@gmail.com>
2018-12-20 15:28:49 -0600

    markymarkup renderBody tests

--- test/processMarkup.js
+++ test/processMarkup.js
@@ -18,7 +18,22 @@ describe('renderBody', function () {
   it('should return bare paragraphs in <p> tags', function () {
     let lorem = marky.renderBody(exampleFiles['loremipsum.txt'])
     assert.equal(lorem, exampleFiles['loremipsum.html'])
+  })
 
+  it('should render single line YAML heds', function () {
+    assert.equal(marky.renderBody('hed: test a headline'), '<h1 class="hed">test a headline</h1>')
+  })
+  
+  it('should render single line HTML heds', function () {
+    assert.equal(marky.renderBody('<h1>test a headline</h1>'),'<h1>test a headline</h1>')
+  })
+
+  it('should render a pair of YAMLs', function () {
+    assert.equal(marky.renderBody('hed: hed\ndek: dek'), '<h1 class="hed">hed</h1>\n<h2 class="dek">dek</h2>')
+  })
+
+  it('should render a mixture of YAML and bare grafs', function () {
+    assert.equal(exampleFiles['loremmixed.html'], marky.renderBody(exampleFiles['loremmixed.txt']))
   })
 
 })
