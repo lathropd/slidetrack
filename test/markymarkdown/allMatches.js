f51d1394df21f8e70e2842566b491e97535ca06c
Daniel Lathrop <daniel.lathrop@gmail.com>
2018-12-20 11:59:38 -0600

    cleanup js style

--- test/markymarkdown/allMatches.js
+++ test/markymarkdown/allMatches.js
@@ -1,31 +1,23 @@
-"use strict"
+'use strict'
 
-const chai = require("chai")
-const assert = chai.assert;
-const allMatches = require("../../lib/markymarkup").allMatches
+const chai = require('chai')
+const assert = chai.assert
+const describe = chai.describe
+const it = chai.it
+const allMatches = require('../../lib/markymarkup').allMatches
 
 // demo tests from https://www.sitepoint.com/unit-test-javascript-mocha-chai/
 
-describe('allMatches', function() {
+describe('allMatches', function () {
   it('should return all the matches', function () {
     let matches = allMatches(/./g, 'abcde')
     assert.equal(matches.length, 5)
-    assert.equal(matches[1][0], "b")
+    assert.equal(matches[1][0], 'b')
   })
 
   it('should convert a non-global regex to a global one', function () {
     let matches = allMatches(/./, 'abcde')
     assert.equal(matches.length, 5)
-    assert.equal(matches[1][0], "b")  
+    assert.equal(matches[1][0], 'b')
   })
-
-  
-
-
-});
-
-
-
-
-
-
+})
