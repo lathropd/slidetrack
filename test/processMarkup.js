79e7112dbd7567c5e4b6bd8e85334b07111d0f29
Daniel Lathrop <daniel.lathrop@gmail.com>
2018-12-21 15:39:32 -0600

    examples, templates and tests for marky.convert

--- test/processMarkup.js
+++ test/processMarkup.js
@@ -78,5 +78,8 @@ describe('renderSection', function () {
 
 // the function used to render the markymarkup into html
 describe('convert', function () {
-  
+  it('should correctly convert a whole markymarkup document into html fragments', function () {
+    let testHTML = marky.convert(exampleFiles['script2.txt'])
+    assert.equal(testHTML, exampleFiles['script2.html'])
+  })
 })
