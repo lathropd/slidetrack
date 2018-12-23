6bfb0fdc06713d9d208b86fddc09652ba0e927d6
Daniel Lathrop <daniel.lathrop@gmail.com>
2018-12-23 15:46:21 -0600

    simplify index.js to separate concerns with the player

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
 'use strict'
-// const waversurfer = require("./lib/wavesurfer")
+// const waversurfer = require("./lib/wavesurfer") // TODO: not working with mr dependency manager
 const slides = require('./lib/slides') // slideshow related functions
 const marky = require('./lib/markymarkup') // markup related functions
 const nunjucks = require('nunjucks')
 
 const slidetrack = function (params) {
-  this.waversurfer = waversurfer // figure this later
+  this.waversurfer = waversurfer // TODO: figure this out later for now should work if waversurfer is already loaded
   this.slides = new slides(params)
-  this.scaffold = () => scaffold(params.player, content)
+  this.scaffold = () => scaffold(params)
   this.convert = marky.convert
 }
 
 // bootstraping functions
 
-function scaffold (playerSelector, content) {
-  let slides
-  // if content is passed as a string
-  if (typeof content === 'string')  {
-    // try it as a query selector
-    let contentElement = document.querySelector(content)
-    // if it is, look for section tags
-    if (typeof contentElement !== null) {
-      if (typeof contentElement.querySelector('section') !== null) {
-        // if so, that element be our slides
-        slides = contentElement.innerHTML
-      } else {
-        // otherwise use its innerHTML to create the slides
-        slides = marky.convert(contentElement.innerHTML)
-      }
-    } else {
-      // use non-selector strings as the html
-      slides = marky.convert(content)
-    }
-  }
-  let player = document.querySelector(playerSelector)
+function scaffold (params) {
+  let player = document.querySelector('#' + params.player)
   if (player === null) {
     player = document.createElement("div")
-    player.id =  'slidetrackPlayer' + Date.now()
-    playerSelector = '#' + player.id
+    player.id = params.player
     player = document.body.append() // TODO: force it to match the selector info
   }
-  player.innerHTML = nunjucks.render('templates/player.njk', {content: slides})
+  player.innerHTML = nunjucks.render('templates/player.njk') // replace this with an inline string later
   return true
 }
 
