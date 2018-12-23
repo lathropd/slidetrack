8ac730507cdd267340a59ae263d280db69078bcd
Daniel Lathrop <daniel.lathrop@gmail.com>
2018-12-23 15:38:10 -0600

    setup index.js to create the slidetrack object

--- index.js
+++ index.js
@@ -1,62 +1,51 @@
-"use strict"
+'use strict'
+// const waversurfer = require("./lib/wavesurfer")
+const slides = require('./lib/slides') // slideshow related functions
+const marky = require('./lib/markymarkup') // markup related functions
+const nunjucks = require('nunjucks')
 
-const marky = require('./lib/markymarkup')
+const slidetrack = function (params) {
+  this.waversurfer = waversurfer // figure this later
+  this.slides = new slides(params)
+  this.scaffold = () => scaffold(params.player, content)
+  this.convert = marky.convert
+}
 
+// bootstraping functions
 
+function scaffold (playerSelector, content) {
+  let slides
+  // if content is passed as a string
+  if (typeof content === 'string')  {
+    // try it as a query selector
+    let contentElement = document.querySelector(content)
+    // if it is, look for section tags
+    if (typeof contentElement !== null) {
+      if (typeof contentElement.querySelector('section') !== null) {
+        // if so, that element be our slides
+        slides = contentElement.innerHTML
+      } else {
+        // otherwise use its innerHTML to create the slides
+        slides = marky.convert(contentElement.innerHTML)
+      }
+    } else {
+      // use non-selector strings as the html
+      slides = marky.convert(content)
+    }
+  }
+  let player = document.querySelector(playerSelector)
+  if (player === null) {
+    player = document.createElement("div")
+    player.id =  'slidetrackPlayer' + Date.now()
+    playerSelector = '#' + player.id
+    player = document.body.append() // TODO: force it to match the selector info
+  }
+  player.innerHTML = nunjucks.render('templates/player.njk', {content: slides})
+  return true
+}
 
-/*
+if (window) {
+  window.slidetrack = slidetrack
+}
 
-comment out to use VS Code's debugger 
-
- const slidey = require("src/js/slidey.js")
-
-
-console.log(slidey);
-
-
-var slideshow = new slidey({});
-
-console.log(slideshow.parentContainer);
-
-console.log(slidey);
-console.log(slideshow);
-
-//// simple code to calculate height offset for each section element
-//var slideHeight = document.getElementsByClassName("slides")[0].offsetHeight;
-//
-//var sections = document.getElementsByTagName("section");
-//for (section in sections) {
-//    console.log(section);
-//    var secHeight = section.offsetHeight;
-//    var secOffset = (slideHeight - secHeight)/2;
-//    if (secOffset < 1) { secOffset = 0}
-//    section.style.top = secOffset+"px"
-//}
-
-console.log("slidey");
-
-slideshow.load("#slidecode");
-
-slideshow.run();
-
-setTimeout(() =>slideshow.next(), 1000);
-setTimeout(() =>slideshow.prev(), 2000);
-
-//setTimeout(() =>slideshow.next(), 3000);
-//setTimeout(() =>slideshow.next(), 6000);
-//setTimeout(() =>slideshow.prev(), 8500);
-//setTimeout(() =>slideshow.prev(), 11000);
-//setTimeout(() =>slideshow.next(), 15000);
-//setTimeout(() => slideshow.goto(0), 17000);
-//setTimeout(() => slideshow.goto(0), 20000);
-//setTimeout(() => slideshow.goto(4), 24000);
-//setTimeout(() => slideshow.goto(1), 28000);
-
-//slideshow.pause();
-
-
-*/
-
-
-
-
+exports.module = slidetrack
